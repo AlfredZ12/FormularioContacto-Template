c1027d75ff19be34d0961cf3d2961102645ef2fd
Jose N <el_fredi17@hotmail.com>
2022-02-18 23:20:00 -0700

    fixed script

--- src/js/maquina.js
+++ src/js/maquina.js
@@ -3,105 +3,76 @@
 let texto = document.getElementById('txt');
 
 const palabras = texto.ariaValueText
+let arrayText = palabras.split(',')
 
-
-
-
-
-const arrayText = palabras.split(',')
 
 console.log(arrayText)
 
 const maquina = (arrayText, tiempo) => {
 
+   
+
+    arrayText = arrayText.map((e)=> {return e+"  "});
     let size = arrayText.length
     let contador = 0;
     let index = 0;
     let estado = true
 
-    // arrayText.forEach(palabra => {
-
-
     setInterval(() => {
-
-
-        if (contador < arrayText.length) {
+        if (contador < size) {
 
             if (index < arrayText[contador].length && estado) {
 
-               // console.log(arrayText[contador].slice(0, index + 1))
-
-                texto.innerText = arrayText[contador].slice(0, index + 1)
-                console.log(index)
+                texto.innerHTML = arrayText[contador].slice(0, index +1) 
                 index++
 
                 if (index > arrayText[contador].length - 1) {
-                    console.log("entro aca")
 
-                    
                     estado = false
-                }
+                
 
-
-
+                    }
             }
 
 
+
+
+
+            if (index > arrayText[contador].length || !estado) {
             
 
-            if (index > arrayText[contador].length - 1 || !estado) {
-                //console.log(arrayText[contador].slice(index, index))
-                //console.log(index)
-                console.log(arrayText[contador].slice(0, index))
-                texto.innerText =arrayText[contador].slice(0, index)
-                console.log(index)
+                texto.innerText = arrayText[contador].slice(0, index) 
+
                 index--
 
-                if (index < 0) {                   
+                if (index < 0) {
+                   
+                    texto.innerText = ""
+                    enfrente=false
                     estado = true;
                     contador++;
-                    index = 0;
-
-
+                    index = 0; 
                 }
-
 
             }
         } else {
             contador = 0
         }
 
-        console.log(contador)
 
+    
 
     }, tiempo)
 
-
-    //});
-
-    // setInterval(()=>{
-    //    console.log( arrayText[contador][0])
-
-    //    arrayText[contador][0]
-
-
-    //     
-
-    //     contador++;
-
-
-    //    },500) 
-
-
-    //
-
-
-    //  
+    
 
 }
 
 
-maquina(arrayText,300)
+maquina(arrayText, 150)
+
+
+
 
 
 
